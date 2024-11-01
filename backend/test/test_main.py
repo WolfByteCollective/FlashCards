670126ee2fc15f3e2e8b8156e12236b25798f67d
priya gandhi <Priya Gandhi>
2024-11-01 19:52:12 -0400

    Update application.yml and Coverage.yml

--- backend/test/test_main.py
+++ backend/test/test_main.py
@@ -1,11 +1,47 @@
+# import sys
+# import unittest
+# import coverage
+# from pathlib import Path
+
+# # Start coverage monitoring
+# cov = coverage.Coverage()
+# cov.start()
+
+# # Add the parent directory to sys.path
+# sys.path.append(str(Path(__file__).parent.parent))
+
+# # Import your test classes
+# from test.test_auth import TestAuth
+# from test.test_deck import TestDeck
+# from test.test_card import CardTestApp
+# from test.test_folders import TestFolders
+
+# def test_suite():
+#     suite = unittest.TestSuite()
+#     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestAuth))
+#     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestDeck))
+#     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(CardTestApp))
+#     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestFolders))
+#     return suite
+
+# if __name__ == '__main__':
+#     runner = unittest.TextTestRunner()
+#     result = runner.run(test_suite())
+    
+#     # Stop coverage monitoring and save results
+#     cov.stop()
+#     cov.save()
+    
+#     # Generate coverage reports
+#     cov.xml_report()
+#     cov.html_report()
+    
+#     sys.exit(not result.wasSuccessful())
+
+# test/test_main.py
 import sys
 import unittest
-import coverage
 from pathlib import Path
-
-# Start coverage monitoring
-cov = coverage.Coverage()
-cov.start()
 
 # Add the parent directory to sys.path
 sys.path.append(str(Path(__file__).parent.parent))
@@ -26,14 +62,4 @@ def test_suite():
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
-    result = runner.run(test_suite())
-    
-    # Stop coverage monitoring and save results
-    cov.stop()
-    cov.save()
-    
-    # Generate coverage reports
-    cov.xml_report()
-    cov.html_report()
-    
-    sys.exit(not result.wasSuccessful())
+    runner.run(test_suite())
